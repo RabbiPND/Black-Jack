83509d23c293dec84fe1333497f5aa25476e8923
Rabbipxd <130862563+RabbiPND@users.noreply.github.com>
2023-05-21 15:21:34 +0200

    unfinished game
    unfinished game

--- Black Jack.js
+++ Black Jack.js
@@ -1,25 +1,50 @@
 let firstCard = 11
 let secondCard = 9
+let sum = firstCard + secondCard
 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-let sum = firstCard + secondCard
-console.log(sum)
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.querySelector("#cards-el")
+let sumEl =document.getElementById("sum-el")
 
-if (sum <= 20) {
-    message = ("Do you want to draw a new card?")
-} else if (sum === 21) {
-    message = ("Wohoo! You have got Blackjack!")
-    hasBlackJack = true
-} else {
-    message = ("You are out of the game")
-    isAlive = false
+
+// console.log(sum)
+
+function startGame () {
+    console.log(renderGame())
 }
 
-// Cash-Out:
-// console.log(hasBlackJack)
-// console.log(isAlive)
+function renderGame() {
 
-console.log(message)
+    cardsEl.textContent = "Cards:" + " " + firstCard + " " + secondCard 
+    sumEl.textContent = "Sum:" + " " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You have got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You are out of the game"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    
+    // Cash-Out:
+    // console.log(hasBlackJack)
+    // console.log(isAlive)
+    
+    // console.log(message)
+}
+
+
+function newCard() {
+    console.log("Drawing a new card from the deck")
+    let add = 8
+    sum = firstCard + secondCard + add
+    console.log(renderGame())
+
+}
