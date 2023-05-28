d43751c9846981267881aa2ca8041383df85982b
Rabbipxd <130862563+RabbiPND@users.noreply.github.com>
2023-05-28 20:29:16 +0200

    Unfinished game
    Unfinished game

--- Black Jack.js
+++ Black Jack.js
@@ -1,26 +1,31 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [
-    firstCard,  // 0
-    secondCard, // 1
-] // Array challenge 1
-let sum = firstCard + secondCard
+let player = {
+    name : "Rabbi",
+    chips : 100 
+}
+
+let cards = [] // Array challenge 1
+let sum = 0
 
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let messageEl = document.getElementById("message-el")
 let cardsEl = document.querySelector("#cards-el")
 let sumEl =document.getElementById("sum-el")
+let playerEl = document.querySelector("#player-el")
+
+console.log(cards)
 
 function getRandomCard (){
-    if (firstCard, secondCard, add === 1) {
-        return 11
-    } else if (firstCard, secondCard, add >= 11) {
+    let randomNumber = Math.floor((Math.random() * 13) + 1);
+    if (randomNumber > 10) {
         return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
     }
-    return Math.floor((Math.random() * 13) + 1);
 }
 
 
@@ -28,7 +33,12 @@ function getRandomCard (){
 // console.log(sum)
 
 function startGame () {
-    console.log(renderGame())
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
 }
 
 function renderGame() {
@@ -61,10 +71,15 @@ function renderGame() {
 
 
 function newCard() {
-    console.log("Drawing a new card from the deck")
-    let add = getRandomCard()
-    cards.push(add)
-    sum = cards[0] + cards[1] + cards[2]
-    console.log(renderGame())
-
+    if (isAlive === true && hasBlackJack === false) {
+        console.log("Drawing a new card from the deck")
+        let add = getRandomCard()
+        cards.push(add)
+        sum += add
+        renderGame()
+    }
 }
+
+playerEl.textContent = player.name + ": R" + player.chips
+
+// let playerEl = document.querySelector("#player-el")
